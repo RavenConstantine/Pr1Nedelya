7649d0777ce49caf5c7f202b35e746a9e4066248
RavenConstantine <50081301+RavenConstantine@users.noreply.github.com>
2022-04-02 05:45:44 +0300

    MiniFix

--- Pr1Nedelya6/Journal.cs
+++ Pr1Nedelya6/Journal.cs
@@ -5,24 +5,20 @@ public class Journal
     private string lastName;
     private string firstName;
     private string patronymic;
-
     public Journal()
     {
-        
     }
-        public Journal( string LastName, string FirstName, string Patronymic)
+    public Journal( string LastName, string FirstName, string Patronymic)
     {
         lastName = LastName;
         firstName = FirstName;
         patronymic = Patronymic;
     }
-
     public string ShowData()
     {
         string str = lastName + " " + firstName + " " + patronymic;
         return str;
     }
-
     public string TakeLName()
     {
         return lastName;
